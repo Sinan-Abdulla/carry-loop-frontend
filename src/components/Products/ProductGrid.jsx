7bb80fb9a78c7a5b1f806498468767cb0940e202
sinan abdulla <sinanabdulla822@gmail.com>
2025-07-27 17:39:30 +0530

    h

---
+++ src/components/Products/ProductGrid.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProductGrid = ({ product }) => {
+  return (
+    <div className="my-20 px-4">
+      {/* Section Heading */}
+      <h2 className="text-2xl font-semibold text-center mb-12">
+        You May Also Like
+      </h2>
+
+      {/* Product Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-7xl mx-auto">
+        {product.map((product) => (
+          <Link key={product._id} to={`/product/${product._id}`} className="block group">
+            <div className="bg-white p-4 rounded-lg shadow-sm hover:shadow-md transition duration-200">
+              <div className="w-full h-72 mb-4 overflow-hidden rounded">
+                <img
+                  src={product.images[0].url}
+                  alt={product.images[0].altText || product.name}
+                  className="w-full h-full object-cover transform group-hover:scale-105 transition duration-300"
+                />
+              </div>
+              <h3 className="text-lg font-semibold mb-1 text-center">{product.name}</h3>
+              <p className="text-gray-700 font-medium text-center">₹{product.price}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductGrid;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Hero from '../components/Layout/Hero'
 import GenderCollectionSection from '../components/Products/GenderCollectionSection'
 import NewArrivaals from '../components/Products/NewArrivaals'
+import ProductDetails from '../components/Products/ProductDetails'
+import { Toaster } from 'sonner'
 
 const Home = () => {
   return (
     <div >
-      <Hero/>
-      <GenderCollectionSection/>
-      <NewArrivaals/>
+      <Toaster position='top-right'/>
+      <Hero />
+      <GenderCollectionSection />
+      <NewArrivaals />
+      {/* best seller */}
+      <h2 className='text-3xl text-center font-bold mb-4'>Best Seller</h2>
+      <ProductDetails />
     </div>
   )
 }
